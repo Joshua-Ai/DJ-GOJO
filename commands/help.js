7c67b2ec860e49936e8572b17448d70d11dbc0ac
KryptoRex <joshuaanil52@gmail.com>
2022-08-17 07:10:06 +0000

    Update 4 files

--- commands/help.js
+++ commands/help.js
@@ -35,11 +35,7 @@ module.exports = {
           GuildDB ? GuildDB.prefix : client.botconfig.DefaultPrefix
         }help [Command] | Have a nice day!`
       ).setDescription(`${Commands.join("\n")}
-  
-  Discord Music Bot Version: v${require("../package.json").version}
-  [✨ Support Server](${
-    client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
@@ -119,11 +115,7 @@ module.exports = {
             GuildDB ? GuildDB.prefix : client.botconfig.DefaultPrefix
           }help [Command] | Have a nice day!`
         ).setDescription(`${Commands.join("\n")}
-  
-  Discord Music Bot Version: v${require("../package.json").version}
-  [✨ Support Server](${
-    client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
